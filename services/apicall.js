3e69932769bb99671c949f02234d920cb72c1973
Carolina Ros <rosdelreal@gmail.com>
2022-02-04 20:27:59 +0100

    Added fetch for API Pokemon List

---
+++ services/apicall.js
@@ -0,0 +1,35 @@
+async function app() {
+    const URL_POKEMON = 'https://pokeapi.co/api/v2/pokemon?limit=25&offset=0';
+    const data = await initiatePokemon();
+    showPokemon(data);
+
+    async function initiatePokemon() {
+        const response = await fetch(URL_POKEMON);
+        return response.json();
+    }
+
+    function showPokemon(data) {
+        console.log(data);
+        const items = data.results;
+        let template = '';
+        template += `<h1>Pokémon</h1>
+                        <img src="./pokemon-logo.svg" alt="Pokemon Logo">
+                        <nav>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Favourites</a></li>
+                        <nav><ul>`;
+
+        items.forEach((itemPokemon) => {
+            template += `
+                <li>
+                    <a href= "">${itemPokemon.name}</a>
+                </li>`;
+        });
+
+        template += `</ul></nav>`;
+        document.querySelector('.pokemon-list').innerHTML += template;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', app);
